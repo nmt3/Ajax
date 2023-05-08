adf5355374a5767513b8b72a3022fa9328968a3d
nmt3 <nmttkm3@gmail.com>
2023-05-08 08:44:03 +0000

    script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ $(function(){
     $.ajax({
       url:"https://api.openweathermap.org/data/2.5/weather?q="+$('#cityname').val()+"&units=metric&appid="+API_KEY,
       dataType:"jsonp",
-    })
+    }).done(function(data){
+      // 通信成功
+    }).fail(function(data){
+      // 通信失敗
+    });
   });
 });
