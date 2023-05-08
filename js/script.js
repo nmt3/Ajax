af349657d5a1c939717308f81d79e855349d62f2
nmt3 <nmttkm3@gmail.com>
2023-05-08 08:38:08 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//(API_KEYには、"取得したAPIキー"を記述)
+const API_KEY="0089666bdf4c1159a6d41d2d1dc7326c";
+
+$(function(){
+  $('#btn').on('click',function(){
+    // 入力された都市名でWebAPIに天気予報をリクエスト
+    $.ajax({
+      url:"https://api.openweathermap.org/data/2.5/weather?q="+$('#cityname').val()+"&units=metric&appid="+API_KEY,
+      dataType:"jsonp",
+    })
+  });
+});
